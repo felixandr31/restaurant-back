6c92461b8e8950b5a23724850eb1eb84b37de43a
Hermann08 <78152998+Hermann08@users.noreply.github.com>
2021-08-30 10:52:59 +0200

    init

---
+++ src/main/java/com/filrouge/restaurantcore/RestaurantCoreApplication.java
@@ -0,0 +1,13 @@
+package com.filrouge.restaurantcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filrouge/restaurantcore/RestaurantCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filrouge.restaurantcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
