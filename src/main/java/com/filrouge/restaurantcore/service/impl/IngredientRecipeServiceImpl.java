7a67ab061dac0e595a35c64a5760a8e45c11437d
ANDRE Felix <felix.andre@activus-group.fr>
2021-09-07 16:18:10 +0200

    add update to ingredientrecipe + validator

--- src/main/java/com/filrouge/restaurantcore/service/impl/IngredientRecipeServiceImpl.java
+++ src/main/java/com/filrouge/restaurantcore/service/impl/IngredientRecipeServiceImpl.java
@@ -7,12 +7,13 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 import com.filrouge.restaurantcore.dao.IIngredientRecipeRepository;
-import com.filrouge.restaurantcore.dto.IngredientDto;
 import com.filrouge.restaurantcore.dto.IngredientRecipeDto;
 import com.filrouge.restaurantcore.entity.IngredientRecipe;
 import com.filrouge.restaurantcore.exception.EntityNotFoundException;
 import com.filrouge.restaurantcore.exception.ErrorCodes;
+import com.filrouge.restaurantcore.exception.InvalidEntityException;
 import com.filrouge.restaurantcore.service.IIngredientRecipeService;
+import com.filrouge.restaurantcore.validator.IngredientRecipeValidator;
 
 /**
  * Services métier de gestion des ingredients d'une recette.
@@ -56,6 +57,30 @@ public class IngredientRecipeServiceImpl implements IIngredientRecipeService {
 	@Override
 	public IngredientRecipeDto save(IngredientRecipeDto dto) {
 		return IngredientRecipeDto.fromEntity(ingredientRecipeRepository.save(IngredientRecipeDto.toEntity(dto)));
+	}
+	
+	
+
+	@Override
+	public IngredientRecipeDto update(IngredientRecipeDto ingredientRecipeDto) {
+		List<String> errors = IngredientRecipeValidator.validate(ingredientRecipeDto);
+		if (!errors.isEmpty()) {
+			throw new InvalidEntityException("le ROLE n'est pas valide", ErrorCodes.ROLE_NOT_VALID,
+					errors);
+		}
+
+		Optional<IngredientRecipe> optionalIngredientRecipe = ingredientRecipeRepository.findById(ingredientRecipeDto.getId());
+
+		if (!optionalIngredientRecipe.isPresent()) {
+			throw new InvalidEntityException("Le ROLE n'existe pas", ErrorCodes.ROLE_NOT_VALID);
+		}
+
+		// Ne mettre à jour que ce dont on a besoin
+		IngredientRecipe toUpdate = optionalIngredientRecipe.get();
+		toUpdate.setQuantity(ingredientRecipeDto.getQuantity());
+//		toUpdate.setIngredient(ingredientRecipeDto.getIngredient()); !!How to IngredientDto and Ingredient
+
+		return IngredientRecipeDto.fromEntity(ingredientRecipeRepository.save(toUpdate));
 	}
 
 	@Override
