d036ada00601d093f6e511e13ca74b482a57480a
Safia <safia.slimani@activus-group.fr>
2021-09-08 16:31:06 +0200

    service booking et resto

--- src/main/java/com/filrouge/restaurantcore/controller/BookingController.java
+++ src/main/java/com/filrouge/restaurantcore/controller/BookingController.java
@@ -1,6 +1,8 @@
 package com.filrouge.restaurantcore.controller;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,4 +55,19 @@ public class BookingController {
 		BookingDto reservationCreated = bookingService.save(bookingDto);
 		return new ResponseEntity<BookingDto>(reservationCreated, HttpStatus.CREATED);
 	}
+
+	@GetMapping(value = "/{tableId}")
+	public ResponseEntity<Collection<BookingDto>> findByTable(@PathVariable("tableId") String tableId) {
+		List<BookingDto> bookingsOfTableX = bookingService.findByTable(tableId);
+		return new ResponseEntity<Collection<BookingDto>>(bookingsOfTableX, HttpStatus.OK);
+	}
+
+	@PostMapping("/addorders/{id}")
+	@Transactional
+	public ResponseEntity<BookingDto> addOrders(@PathVariable String id, @RequestBody Set<String> ordersIds) {
+
+		BookingDto bookingUpdate = bookingService.addOrders(id, ordersIds);
+		return new ResponseEntity<BookingDto>(bookingUpdate, HttpStatus.CREATED);
+	}
+
 }
